01ed27d48bafbe72279a597376a2d501166828d3
sujitpawar <sujitpawar074@gmail.com>
2026-01-28 13:16:54 +0530

    # 2 Created Employee Form. Created Add, Delete, Update feature using redux, usedipatch and useSelector hook.

--- UI/employee_management/src/App.jsx
+++ UI/employee_management/src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
+import AddEmployee from "./pages/AddEmployee";
 import Dashboard from "./pages/Dashboard";
+import { Routes, Route } from "react-router-dom";
+import EmployeeForm from "./pages/EmployeeForm";
 
 function App() {
   return (
-    <>
-      <Dashboard />
-    </>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/add-employee" element={<AddEmployee />} />
+      <Route path="/employee" element={<EmployeeForm />} />
+      <Route path="/employee/:id" element={<EmployeeForm />} />
+    </Routes>
   );
 }
 
